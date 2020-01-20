95a561cd787f03b80b5ce23a95361c0022ebbf95
Luke Oliff <luke@lukeoliff.com>
2020-01-20 10:06:29 -0500

    remove extra condition that was added by mistake

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -118,32 +118,30 @@ class ChatApp {
       this.scrollHeight = this.messageFeed.scrollHeight;
 
       if (this.isFeedAtTop() && this.lastPage.hasNext()) {
-        if (this.lastPage.hasNext()) {
-          this.lastPage
-            .getNext()
-            .then((eventsPage) => {
-              this.lastPage = eventsPage;
-              var moreEvents = "";
+        this.lastPage
+          .getNext()
+          .then((eventsPage) => {
+            this.lastPage = eventsPage;
+            var moreEvents = "";
 
-              eventsPage.items.forEach((value, key) => {
-                if (this.conversation.members.get(value.from)) {
-                  switch (value.type) {
-                    case 'text':
-                      moreEvents = this.senderMessage(this.user, this.conversation.members.get(value.from), value) + moreEvents;
-                      break;
-                    case 'member:joined':
-                      moreEvents = this.memberJoined(this.conversation.members.get(value.from), value) + moreEvents;
-                      break;
-                  }
+            eventsPage.items.forEach((value, key) => {
+              if (this.conversation.members.get(value.from)) {
+                switch (value.type) {
+                  case 'text':
+                    moreEvents = this.senderMessage(this.user, this.conversation.members.get(value.from), value) + moreEvents;
+                    break;
+                  case 'member:joined':
+                    moreEvents = this.memberJoined(this.conversation.members.get(value.from), value) + moreEvents;
+                    break;
                 }
-              });
+              }
+            });
 
-              this.messageFeed.innerHTML = moreEvents + this.messageFeed.innerHTML;
+            this.messageFeed.innerHTML = moreEvents + this.messageFeed.innerHTML;
 
-              this.scrollTo(this.messageFeed.scrollHeight-this.scrollHeight);
-            })
-            .catch(this.errorLogger);
-        }
+            this.scrollTo(this.messageFeed.scrollHeight-this.scrollHeight);
+          })
+          .catch(this.errorLogger);
       }
     });
   }
